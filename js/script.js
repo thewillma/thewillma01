a10898b033f103ae61625a4244f549d96ab9dae2
thewillma <145166652+thewillma@users.noreply.github.com>
2023-09-15 22:21:44 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let login = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () =>{
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector('#menu-btn').onclick = () =>{
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    login.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+  
